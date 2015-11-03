5ca00e48202022f59b6af19742b9bbc8a3a532e0
Mark Radocy <mradocy@gmail.com>
2015-11-02 19:18:11 -0500

    Made options page its own prefab

---
+++ Assets/OptionsPage.cs
@@ -0,0 +1,270 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OptionsPage : MonoBehaviour {
+
+    public AudioClip switchSound;
+
+    public void update() {
+
+        optionsUpdate();
+
+        bool madeSelection = false;
+        Text option = null;
+        if (settingSFX || settingMusic) {
+            if (Input.GetButtonDown("Left")) {
+                if (settingSFX) {
+                    Vars.sfxVolume = Mathf.Max(0, Vars.sfxVolume - .2f);
+                    setVolumeText(true);
+                } else {
+                    Vars.musicVolume = Mathf.Max(0, Vars.musicVolume - .2f);
+                    setVolumeText(false);
+                }
+                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
+            } else if (Input.GetButtonDown("Right")) {
+                if (settingSFX) {
+                    Vars.sfxVolume = Mathf.Min(1, Vars.sfxVolume + .2f);
+                    setVolumeText(true);
+                } else {
+                    Vars.musicVolume = Mathf.Min(1, Vars.musicVolume + .2f);
+                    setVolumeText(false);
+                }
+                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
+            }
+            if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1")) {
+                // exit out of setting SFX/Music
+                bool temp = settingSFX;
+                show();
+                if (temp)
+                    setSelection(options.IndexOf(sfxVolumeText), true);
+                else
+                    setSelection(options.IndexOf(musicVolumeText), true);
+            }
+        } else {
+            if (Input.GetButtonDown("Jump")) {
+                madeSelection = true;
+                option = options[selectionIndex];
+            }
+            if (Input.GetButtonDown("Fire1")) {
+                if (quitSureNoText.enabled) {
+                    madeSelection = true;
+                    option = quitSureNoText;
+                }
+            }
+        }
+        if (madeSelection) { // pressed selection
+            if (option == resumeText) {
+                // resume game
+                PauseScreen.instance.unpauseGame();
+            } else if (option == restartText) {
+                // restart game
+                PauseScreen.instance.unpauseGame();
+                Vars.restartLevel();
+            } else if (option == sfxVolumeText) {
+                // toggle sfx volue
+                option.color = PauseScreen.DEFAULT_COLOR;
+                hide();
+                volumeText.enabled = true;
+                setVolumeText(true);
+                options.Clear();
+                settingSFX = true;
+
+            } else if (option == musicVolumeText) {
+                // toggle music volue
+                option.color = PauseScreen.DEFAULT_COLOR;
+                hide();
+                volumeText.enabled = true;
+                setVolumeText(false);
+                options.Clear();
+                settingMusic = true;
+
+            } else if (option == quitText) {
+                // go to quit sure mode
+                // get rid of other options
+                option.color = PauseScreen.DEFAULT_COLOR;
+                hide();
+                selection.enabled = true;
+                quitSureYesText.rectTransform.localPosition = options[1].rectTransform.localPosition;
+                quitSureNoText.rectTransform.localPosition = options[2].rectTransform.localPosition;
+                options.Clear();
+                // set new options
+                quitSureText.enabled = true;
+                quitSureYesText.enabled = true;
+                quitSureNoText.enabled = true;
+                options.Add(quitSureYesText);
+                options.Add(quitSureNoText);
+                setSelection(1, true);
+            } else if (option == quitSureNoText) {
+                // cancel quitting
+                show();
+                setSelection(options.IndexOf(quitText), true);
+            } else {
+                // quit game (should quit to title screen)
+                #if UNITY_EDITOR
+                // set the PlayMode to stop
+                #else
+                Application.Quit();
+                #endif
+            }
+
+        }
+
+    }
+	
+	void Awake() {
+        selection = transform.Find("Selection").GetComponent<Image>();
+        resumeText = transform.Find("ResumeText").GetComponent<Text>();
+        restartText = transform.Find("RestartText").GetComponent<Text>();
+        sfxVolumeText = transform.Find("sfxVolumeText").GetComponent<Text>();
+        musicVolumeText = transform.Find("MusicVolumeText").GetComponent<Text>();
+        volumeText = transform.Find("VolumeText").GetComponent<Text>();
+        quitText = transform.Find("QuitText").GetComponent<Text>();
+        quitSureText = transform.Find("QuitSureText").GetComponent<Text>();
+        quitSureYesText = transform.Find("QuitSureYesText").GetComponent<Text>();
+        quitSureNoText = transform.Find("QuitSureNoText").GetComponent<Text>();
+	}
+	
+	void Update() {
+		
+	}
+
+
+    public void show() {
+        selection.enabled = true;
+        resumeText.enabled = true;
+        restartText.enabled = true;
+        sfxVolumeText.enabled = true;
+        musicVolumeText.enabled = true;
+        volumeText.enabled = false;
+        quitText.enabled = true;
+        quitSureText.enabled = false;
+        quitSureYesText.enabled = false;
+        quitSureNoText.enabled = false;
+        settingSFX = false;
+        settingMusic = false;
+        // set options helper
+        selectionImage = selection;
+        selectionImageOffset.Set(0, 2);
+        options.Clear();
+        options.Add(resumeText);
+        options.Add(restartText);
+        options.Add(sfxVolumeText);
+        options.Add(musicVolumeText);
+        options.Add(quitText);
+        setSelection(0, true);
+    }
+
+    public void hide() {
+        selection.enabled = false;
+        resumeText.enabled = false;
+        restartText.enabled = false;
+        sfxVolumeText.enabled = false;
+        musicVolumeText.enabled = false;
+        volumeText.enabled = false;
+        quitText.enabled = false;
+        quitSureText.enabled = false;
+        quitSureYesText.color = PauseScreen.DEFAULT_COLOR;
+        quitSureYesText.enabled = false;
+        quitSureNoText.color = PauseScreen.DEFAULT_COLOR;
+        quitSureNoText.enabled = false;
+    }
+
+    // call this during update to move the selection around
+    void optionsUpdate() {
+        // button presses to navigate options
+        if (options.Count > 0) {
+            if (Input.GetButtonDown("Down")) {
+                setSelection(selectionIndex + 1);
+                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
+            } else if (Input.GetButtonDown("Up")) {
+                setSelection(selectionIndex - 1);
+                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
+            }
+        }
+        // move selection image
+        float selectionTransDur = .1f;
+        if (selectionImageTime < selectionTransDur) {
+            selectionImageTime += Time.unscaledDeltaTime;
+            selectionImage.rectTransform.localPosition = new Vector3(
+                Utilities.easeOutQuadClamp(selectionImageTime, selectionImageInitialPos.x, selectionImageFinalPos.x - selectionImageInitialPos.x, selectionTransDur),
+                Utilities.easeOutQuadClamp(selectionImageTime, selectionImageInitialPos.y, selectionImageFinalPos.y - selectionImageInitialPos.y, selectionTransDur)
+                );
+        }
+    }
+
+    void setVolumeText(bool sfx) {
+        float val = 0;
+        if (sfx) val = Vars.sfxVolume;
+        else val = Vars.musicVolume;
+        string left = "<";
+        if (val <= .0001f) left = " ";
+        string right = ">";
+        if (val >= .9999f) right = " ";
+        int ones = Mathf.FloorToInt(val);
+        string str = "" + ones;
+        val -= ones;
+        str += "." + Mathf.FloorToInt(val * 10 + .01f);
+        if (sfx) {
+            volumeText.text = "SFX Volume: " + left + " " + str + " " + right;
+        } else {
+            volumeText.text = "Music Volume: " + left + " " + str + " " + right;
+        }
+    }
+
+    // options helpers
+    /* Set options, selectionImage, selectionImageOffset then call setSelection() */
+    List<Text> options = new List<Text>();
+    Image selectionImage = null;
+    Vector2 selectionImageOffset = new Vector2();
+    int selectionIndex = -1;
+    float selectionImageTime = 9999;
+    Vector2 selectionImageInitialPos = new Vector2();
+    Vector2 selectionImageFinalPos = new Vector2();
+    void setSelection(int index, bool immediately = false) {
+
+        Text option;
+        if (selectionIndex >= 0 && selectionIndex < options.Count) {
+            // unselect
+            option = options[selectionIndex];
+            option.color = PauseScreen.DEFAULT_COLOR;
+        }
+
+        // select
+        if (index < 0)
+            index = options.Count - 1;
+        else if (index >= options.Count)
+            index = 0;
+        if (selectionIndex >= 0 && selectionIndex < options.Count) {
+            option = options[selectionIndex]; //previously selected
+            selectionImageInitialPos.Set(selectionImage.rectTransform.localPosition.x, selectionImage.rectTransform.localPosition.y);
+        } else {
+            immediately = true;
+        }
+        selectionImageTime = 0;
+        selectionIndex = index;
+        option = options[selectionIndex];
+        option.color = PauseScreen.SELECTED_COLOR;
+        selectionImageFinalPos = selectionImageOffset + new Vector2(option.rectTransform.localPosition.x, option.rectTransform.localPosition.y);
+        if (immediately) {
+            selectionImageInitialPos = selectionImageFinalPos;
+        }
+    }
+
+    Image selection;
+    Text resumeText;
+    Text restartText; // only use in Arcade
+
+    Text sfxVolumeText;
+    Text musicVolumeText;
+    Text volumeText;
+
+    Text quitText;
+    Text quitSureText;
+    Text quitSureYesText;
+    Text quitSureNoText;
+    bool settingSFX = false;
+    bool settingMusic = false;
+
+}
--- Assets/code/PauseScreen.cs
+++ Assets/code/PauseScreen.cs
@@ -9,6 +9,9 @@ public class PauseScreen : MonoBehaviour {
 
     public AudioClip switchSound;
 
+    public static Color DEFAULT_COLOR = new Color(203 / 255f, 136 / 255f, 177 / 255f);
+    public static Color SELECTED_COLOR = new Color(45 / 255f, 0 / 255f, 27 / 255f);
+
     public enum Page {
         NONE, //meaning entire pause screen is hidden
         MAP,
@@ -59,17 +62,17 @@ public class PauseScreen : MonoBehaviour {
         _paused = false;
     }
     public void initialHide() {
-        m_hide();
-        tt_hide();
-        t_hide();
-        p_hide();
-        o_hide();
+        //mapPage.hide();
+        //timeTreePage.hide();
+        //talkPage.hide();
+        //progressPage.hide();
+        optionsPage.hide();
         hide();
     }
 
-
-    public static Color DEFAULT_COLOR = new Color(203 / 255f, 136 / 255f, 177 / 255f);
-    public static Color SELECTED_COLOR = new Color(45 / 255f, 0 / 255f, 27 / 255f);
+    /////////////
+    // PRIVATE //
+    /////////////
 
     void show(Page page) {
         image.enabled = true;
@@ -110,23 +113,23 @@ public class PauseScreen : MonoBehaviour {
         switch (page) {
         case Page.MAP:
             option = mapPageText;
-            m_hide();
+            //mapPage.hide();
             break;
         case Page.TIME_TREE:
             option = timeTreePageText;
-            tt_hide();
+            //timeTreePage.hide();
             break;
         case Page.TALK:
             option = talkPageText;
-            t_hide();
+            //talkPage.hide();
             break;
         case Page.PROGRESS:
             option = progressPageText;
-            p_hide();
+            //progressPage.hide();
             break;
         case Page.OPTIONS:
             option = optionsPageText;
-            o_hide();
+            optionsPage.hide();
             break;
         }
         if (option == null) {
@@ -140,23 +143,23 @@ public class PauseScreen : MonoBehaviour {
         switch (page) {
         case Page.MAP:
             option = mapPageText;
-            m_show();
+            //mapPage.show();
             break;
         case Page.TIME_TREE:
             option = timeTreePageText;
-            tt_show();
+            //timeTreePage.show();
             break;
         case Page.TALK:
             option = talkPageText;
-            t_show();
+            //talkPage.show();
             break;
         case Page.PROGRESS:
             option = progressPageText;
-            p_show();
+            //progressPage.show();
             break;
         case Page.OPTIONS:
             option = optionsPageText;
-            o_show();
+            optionsPage.show();
             break;
         }
         if (option != null) {
@@ -193,17 +196,7 @@ public class PauseScreen : MonoBehaviour {
         switchPagesText = transform.Find("SwitchPagesText").GetComponent<Text>();
 
         // options page
-        optionsPage = transform.Find("OptionsPage").gameObject;
-        o_selection = optionsPage.transform.Find("Selection").GetComponent<Image>();
-        o_resumeText = optionsPage.transform.Find("ResumeText").GetComponent<Text>();
-        o_restartText = optionsPage.transform.Find("RestartText").GetComponent<Text>();
-        o_sfxVolumeText = optionsPage.transform.Find("sfxVolumeText").GetComponent<Text>();
-        o_musicVolumeText = optionsPage.transform.Find("MusicVolumeText").GetComponent<Text>();
-        o_volumeText = optionsPage.transform.Find("VolumeText").GetComponent<Text>();
-        o_quitText = optionsPage.transform.Find("QuitText").GetComponent<Text>();
-        o_quitSureText = optionsPage.transform.Find("QuitSureText").GetComponent<Text>();
-        o_quitSureYesText = optionsPage.transform.Find("QuitSureYesText").GetComponent<Text>();
-        o_quitSureNoText = optionsPage.transform.Find("QuitSureNoText").GetComponent<Text>();
+        optionsPage = transform.Find("OptionsPage").GetComponent<OptionsPage>();
 
 	}
 	
@@ -258,7 +251,9 @@ public class PauseScreen : MonoBehaviour {
 
         // page update functions
         switch (page) {
-        case Page.OPTIONS: o_update(); break;
+        case Page.OPTIONS:
+            optionsPage.update();
+            break;
         }
         
         // unpausing game
@@ -284,68 +279,7 @@ public class PauseScreen : MonoBehaviour {
     Vector2 pageSelectionImageInitialPos = new Vector2();
     Vector2 pageSelectionImageFinalPos = new Vector2();
 
-    // options helpers
-    /* Set options, selectionImage, selectionImageOffset then call setSelection() */
-    List<Text> options = new List<Text>();
-    Image selectionImage = null;
-    Vector2 selectionImageOffset = new Vector2();
-    int selectionIndex = -1;
-    float selectionImageTime = 9999;
-    Vector2 selectionImageInitialPos = new Vector2();
-    Vector2 selectionImageFinalPos = new Vector2();
-    void setSelection(int index, bool immediately = false) {
-        
-        Text option;
-        if (selectionIndex >= 0 && selectionIndex < options.Count) {
-            // unselect
-            option = options[selectionIndex];
-            option.color = DEFAULT_COLOR;
-        }
-
-        // select
-        if (index < 0)
-            index = options.Count - 1;
-        else if (index >= options.Count)
-            index = 0;
-        if (selectionIndex >= 0 && selectionIndex < options.Count) {
-            option = options[selectionIndex]; //previously selected
-            selectionImageInitialPos.Set(selectionImage.rectTransform.localPosition.x, selectionImage.rectTransform.localPosition.y);
-        } else {
-            immediately = true;
-        }
-        selectionImageTime = 0;
-        selectionIndex = index;
-        option = options[selectionIndex];
-        option.color = SELECTED_COLOR;
-        selectionImageFinalPos = selectionImageOffset + new Vector2(option.rectTransform.localPosition.x, option.rectTransform.localPosition.y);
-        if (immediately) {
-            selectionImageInitialPos = selectionImageFinalPos;
-        }
-    }
-    // call this during update to move the selection around
-    void optionsUpdate() {
-        // button presses to navigate options
-        if (options.Count > 0) {
-            if (Input.GetButtonDown("Down")) {
-                setSelection(selectionIndex + 1);
-                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
-            } else if (Input.GetButtonDown("Up")) {
-                setSelection(selectionIndex - 1);
-                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
-            }
-        }
-        // move selection image
-        float selectionTransDur = .1f;
-        if (selectionImageTime < selectionTransDur) {
-            selectionImageTime += Time.unscaledDeltaTime;
-            selectionImage.rectTransform.localPosition = new Vector3(
-                Utilities.easeOutQuadClamp(selectionImageTime, selectionImageInitialPos.x, selectionImageFinalPos.x-selectionImageInitialPos.x, selectionTransDur),
-                Utilities.easeOutQuadClamp(selectionImageTime, selectionImageInitialPos.y, selectionImageFinalPos.y-selectionImageInitialPos.y, selectionTransDur)
-                );
-        }
-    }
-
-	// main pause screen
+    // main pause screen
     Image image;
     Text mapPageText;
     Text timeTreePageText;
@@ -355,204 +289,24 @@ public class PauseScreen : MonoBehaviour {
     Image pageSelection;
     Text switchPagesText;
     
-    // map page
-    void m_show() { }
-    void m_hide() { }
-
-    // time tree page
-    void tt_show() { }
-    void tt_hide() { }
-
     // talk page
     /* Talking should be able to be visible while displaying the map or time tree (for tutorials, etc.)
      * Remember the talk page will have secrets:
      *   - Holding FLASHBACK to rewind text
      *   - Tap __ to start a small minigame
      * */
-    void t_show() { }
-    void t_hide() { }
-
-    // progress page
-    void p_show() { }
-    void p_hide() { }
-
-    // options page
-    GameObject optionsPage;
-    Image o_selection;
-    Text o_resumeText;
-    Text o_restartText; // only use in Arcade
-
-    Text o_sfxVolumeText;
-    Text o_musicVolumeText;
-    Text o_volumeText;
-
-    Text o_quitText;
-    Text o_quitSureText;
-    Text o_quitSureYesText;
-    Text o_quitSureNoText;
-    bool o_settingSFX = false;
-    bool o_settingMusic = false;
-    void o_show() {
-        o_selection.enabled = true;
-        o_resumeText.enabled = true;
-        o_restartText.enabled = true;
-        o_sfxVolumeText.enabled = true;
-        o_musicVolumeText.enabled = true;
-        o_volumeText.enabled = false;
-        o_quitText.enabled = true;
-        o_quitSureText.enabled = false;
-        o_quitSureYesText.enabled = false;
-        o_quitSureNoText.enabled = false;
-        o_settingSFX = false;
-        o_settingMusic = false;
-        // set options helper
-        selectionImage = o_selection;
-        selectionImageOffset.Set(0, 2);
-        options.Clear();
-        options.Add(o_resumeText);
-        options.Add(o_restartText);
-        options.Add(o_sfxVolumeText);
-        options.Add(o_musicVolumeText);
-        options.Add(o_quitText);
-        setSelection(0, true);
-    }
-    void o_hide() {
-        o_selection.enabled = false;
-        o_resumeText.enabled = false;
-        o_restartText.enabled = false;
-        o_sfxVolumeText.enabled = false;
-        o_musicVolumeText.enabled = false;
-        o_volumeText.enabled = false;
-        o_quitText.enabled = false;
-        o_quitSureText.enabled = false;
-        o_quitSureYesText.color = DEFAULT_COLOR;
-        o_quitSureYesText.enabled = false;
-        o_quitSureNoText.color = DEFAULT_COLOR;
-        o_quitSureNoText.enabled = false;
-    }
-    void o_update() {
-
-        optionsUpdate();
-
-        bool madeSelection = false;
-        Text option = null;
-        if (o_settingSFX || o_settingMusic) {
-            if (Input.GetButtonDown("Left")) {
-                if (o_settingSFX) {
-                    Vars.sfxVolume = Mathf.Max(0, Vars.sfxVolume - .2f);
-                    o_setVolumeText(true);
-                } else {
-                    Vars.musicVolume = Mathf.Max(0, Vars.musicVolume - .2f);
-                    o_setVolumeText(false);
-                }
-                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
-            } else if (Input.GetButtonDown("Right")) {
-                if (o_settingSFX) {
-                    Vars.sfxVolume = Mathf.Min(1, Vars.sfxVolume + .2f);
-                    o_setVolumeText(true);
-                } else {
-                    Vars.musicVolume = Mathf.Min(1, Vars.musicVolume + .2f);
-                    o_setVolumeText(false);
-                }
-                SoundManager.instance.playSFXIgnoreVolumeScale(switchSound);
-            }
-            if (Input.GetButtonDown("Jump") || Input.GetButtonDown("Fire1")) {
-                // exit out of setting SFX/Music
-                bool temp = o_settingSFX;
-                o_show();
-                if (temp)
-                    setSelection(options.IndexOf(o_sfxVolumeText), true);
-                else
-                    setSelection(options.IndexOf(o_musicVolumeText), true);
-            }
-        } else {
-            if (Input.GetButtonDown("Jump")) {
-                madeSelection = true;
-                option = options[selectionIndex];
-            }
-            if (Input.GetButtonDown("Fire1")) {
-                if (o_quitSureNoText.enabled) {
-                    madeSelection = true;
-                    option = o_quitSureNoText;
-                }
-            }
-        }
-        if (madeSelection) { // pressed selection
-            if (option == o_resumeText) {
-                // resume game
-                unpauseGame();
-            } else if (option == o_restartText) {
-                // restart game
-                unpauseGame();
-                Vars.restartLevel();
-            } else if (option == o_sfxVolumeText) {
-                // toggle sfx volue
-                option.color = DEFAULT_COLOR;
-                o_hide();
-                o_volumeText.enabled = true;
-                o_setVolumeText(true);
-                options.Clear();
-                o_settingSFX = true;
-
-            } else if (option == o_musicVolumeText) {
-                // toggle music volue
-                option.color = DEFAULT_COLOR;
-                o_hide();
-                o_volumeText.enabled = true;
-                o_setVolumeText(false);
-                options.Clear();
-                o_settingMusic = true;
-
-            } else if (option == o_quitText) {
-                // go to quit sure mode
-                // get rid of other options
-                option.color = DEFAULT_COLOR;
-                o_hide();
-                o_selection.enabled = true;
-                o_quitSureYesText.rectTransform.localPosition = options[1].rectTransform.localPosition;
-                o_quitSureNoText.rectTransform.localPosition = options[2].rectTransform.localPosition;
-                options.Clear();
-                // set new options
-                o_quitSureText.enabled = true;
-                o_quitSureYesText.enabled = true;
-                o_quitSureNoText.enabled = true;
-                options.Add(o_quitSureYesText);
-                options.Add(o_quitSureNoText);
-                setSelection(1, true);
-            } else if (option == o_quitSureNoText) {
-                // cancel quitting
-                o_show();
-                setSelection(options.IndexOf(o_quitText), true);
-            } else {
-                // quit game (should quit to title screen)
-                #if UNITY_EDITOR
-                // set the PlayMode to stop
-                #else
-                Application.Quit();
-                #endif 
-            }
-
-        }
-
-    }
-    void o_setVolumeText(bool sfx) {
-        float val = 0;
-        if (sfx) val = Vars.sfxVolume;
-        else val = Vars.musicVolume;
-        string left = "<";
-        if (val <= .0001f) left = " ";
-        string right = ">";
-        if (val >= .9999f) right = " ";
-        int ones = Mathf.FloorToInt(val);
-        string str = "" + ones;
-        val -= ones;
-        str += "." + Mathf.FloorToInt(val*10 + .01f);
-        if (sfx) {
-            o_volumeText.text = "SFX Volume: " + left + " " + str + " " + right;
-        } else {
-            o_volumeText.text = "Music Volume: " + left + " " + str + " " + right;
-        }
-    }
+    
+    // pages
+    //MapPage mapPage;
+    //TimeTreePage timeTreePage
+    //TalkPage talkPage;
+    //ProgressPage progressPage;
+    OptionsPage optionsPage;
+    
+    
+    
+    
+    
 
 
 }
