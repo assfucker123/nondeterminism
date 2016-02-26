14787fb51bf4659da5a8a767fc25467642e78627
mradocy <mradocy@gmail.com>
2016-02-25 21:58:27 -0600

    Fixed up leaves tileset, created Ambush code files.

---
+++ Assets/Ambush.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ambush : MonoBehaviour {
+
+    public bool useIncludedSensor = false; // it's preferred to use AmbushTrigger
+
+    public bool activated {  get { return state == State.ACTIVATED; } }
+
+    public enum State {
+        NOT_ACTIVE,
+        ACTIVATED
+    }
+
+    public void activate() {
+        if (activated) return;
+
+        Debug.Log("WORK HERE on ambush");
+
+        state = State.ACTIVATED;
+    }
+
+	void Awake() {
+        timeUser = GetComponent<TimeUser>();
+	}
+	
+	void Update() {
+
+        if (timeUser.shouldNotUpdate)
+            return;
+	}
+
+    void OnSaveFrame(FrameInfo fi) {
+        fi.state = (int)state;
+    }
+
+    void OnRevert(FrameInfo fi) {
+        state = (State)fi.state;
+    }
+
+    void OnTriggerEnter2D(Collider2D c2d) {
+        if (!useIncludedSensor) return;
+        if (c2d.gameObject == null) return;
+        if (c2d.gameObject != Player.instance.gameObject) return;
+
+        activate();
+    }
+
+    TimeUser timeUser;
+
+    State state = State.NOT_ACTIVE;
+}
---
+++ Assets/AmbushSensor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* triggers an ambush when player triggers the sensor */
+public class AmbushSensor : MonoBehaviour {
+
+    public GameObject ambushRef = null; // if left null, will activate closest ambush
+
+	void Awake() {
+		
+	}
+	
+	void Update() {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D c2d) {
+        if (c2d.gameObject == null) return;
+        if (c2d.gameObject != Player.instance.gameObject) return;
+        if (TimeUser.reverting) return;
+
+        if (ambushRef == null)
+            ambushRef = findClosestAmbush();
+        if (ambushRef == null)
+            return;
+        if (ambushRefA == null)
+            ambushRefA = ambushRef.GetComponent<Ambush>();
+        if (ambushRefA == null)
+            return;
+
+        ambushRefA.activate();
+    }
+
+    GameObject findClosestAmbush() {
+        Ambush[] ambushes = GameObject.FindObjectsOfType<Ambush>();
+        if (ambushes.Length == 0) return null;
+        if (ambushes.Length == 1) return ambushes[0].gameObject;
+        Ambush ret = ambushes[0];
+        float dist = Vector3.SqrMagnitude(ret.transform.localPosition - transform.localPosition);
+        foreach (Ambush ambush in ambushes) {
+            float d = Vector3.SqrMagnitude(ambush.transform.localPosition - transform.localPosition);
+            if (d < dist) {
+                dist = d;
+                ret = ambush;
+            }
+        }
+        return ret.gameObject;
+    }
+
+    Ambush ambushRefA = null;
+
+}
