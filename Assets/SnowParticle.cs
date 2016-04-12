148ee345dfdaba75fe5ce822214259ddf7073604
mradocy <mradocy@gmail.com>
2016-04-11 22:57:10 -0500

    Added snow

---
+++ Assets/SnowParticle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowParticle : MonoBehaviour {
+
+    public Color color1 = new Color();
+    public Color color2 = new Color();
+    public Sprite[] particles;
+    [HideInInspector]
+    public Vector2 startPos = new Vector2();
+    public float heading {
+        get { return _heading; }
+        set {
+            _heading = value;
+        }
+    }
+    [HideInInspector]
+    public float speed = 30;
+    [HideInInspector]
+    public float waveTimeOffset = 0;
+    public float waveMagnitude = 2;
+    public float wavePeriod = 2;
+
+
+    public void setSprite() {
+        setSprite(Random.Range(0, particles.Length), Color.Lerp(color1, color2, Random.value));
+    }
+    public void setSprite(int particleIndex, Color color) {
+        spriteRenderer.sprite = particles[particleIndex];
+        spriteRenderer.color = color;
+    }
+
+    /// <summary>
+    /// Calculates position over time.
+    /// </summary>
+    public Vector2 getPosition(float time) {
+        Vector2 pos = new Vector2();
+        pos.x = speed * time;
+        pos.y = waveMagnitude * Mathf.Sin((time + waveTimeOffset) * Mathf.PI * 2 / wavePeriod);
+        pos = Utilities.rotateAroundPoint(pos, Vector2.zero, heading * Mathf.Deg2Rad);
+        pos += startPos;
+        return pos;
+    }
+
+	void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+    SpriteRenderer spriteRenderer;
+    float _heading = 0;
+}
---
+++ Assets/SnowParticles.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(TimeUser))]
+public class SnowParticles : MonoBehaviour {
+
+    public float particleDensity = .03f;
+    public float particleHeading = -90;
+    public float particleHeadingSpread = 10;
+    public float particleSpeedMin = 5;
+    public float particleSpeedMax = 10;
+    public float particleWaveMagnitudeMin = 3;
+    public float particleWaveMagnitudeMax = 2;
+    public float particleWavePeriodMin = 2;
+    public float particleWavePeriodMax = 2;
+    public GameObject snowParticleGameObject;
+
+	void Awake() {
+        timeUser = GetComponent<TimeUser>();
+        // immediately reposition so that it's in center of the map
+        Rect bounds = CameraControl.getMapBounds();
+        width = bounds.width + 2;
+        height = bounds.height + 2;
+        transform.localPosition = bounds.min - new Vector2(1, 1);
+        numParticles = Mathf.RoundToInt(width * height * particleDensity);
+	}
+
+    void Start() {
+        // create particles
+        while (particles.Count < numParticles) {
+            Vector2 startPos = new Vector2(Random.Range(0, width), Random.Range(0, height));
+            GameObject pGO = GameObject.Instantiate(snowParticleGameObject, startPos, Quaternion.identity) as GameObject;
+            pGO.transform.SetParent(transform, false);
+            SnowParticle part = pGO.GetComponent<SnowParticle>();
+            part.startPos = startPos;
+            part.speed = Random.Range(particleSpeedMin, particleSpeedMax);
+            part.heading = particleHeading + Random.Range(-particleHeadingSpread / 2, particleHeadingSpread / 2);
+            part.waveMagnitude = Random.Range(particleWaveMagnitudeMin, particleWaveMagnitudeMax);
+            part.wavePeriod = Random.Range(particleWavePeriodMin, particleWavePeriodMax);
+            part.waveTimeOffset = Random.Range(0, part.wavePeriod);
+            part.setSprite();
+
+            particles.Add(part);
+        }
+    }
+	
+	void Update() {
+
+        if (timeUser.shouldNotUpdate)
+            return;
+
+        time += Time.deltaTime;
+
+        setParticlePositions();
+
+	}
+
+    void setParticlePositions() {
+
+        foreach (SnowParticle part in particles) {
+            Vector2 pos = part.getPosition(time);
+            // fit pos in box
+            pos.x = Utilities.fmod(pos.x, width);
+            pos.y = Utilities.fmod(pos.y, height);
+            // set position
+            part.gameObject.transform.localPosition = pos;
+        }
+
+    }
+
+    void OnSaveFrame(FrameInfo fi) {
+        fi.floats["t"] = time;
+    }
+
+    void OnRevert(FrameInfo fi) {
+        time = fi.floats["t"];
+        setParticlePositions();
+    }
+
+
+    void OnDestroy() {
+        foreach (SnowParticle part in particles) {
+            GameObject.Destroy(part.gameObject);
+        }
+        particles.Clear();
+    }
+
+    List<SnowParticle> particles = new List<SnowParticle>();
+
+    TimeUser timeUser;
+    float time = 0;
+
+    float width = 42;
+    float height = 23;
+    int numParticles = 20;
+}
